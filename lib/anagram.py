abbe3912b54894d9fc81a9368a44cb8a72b10ec7
florence wangechi <florencewangechim@gmail.com>
2023-05-23 22:32:22 +0300

    Anagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,17 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, candidates):
+        matches = []
+        sorted_word = sorted(self.word)
+
+        for candidate in candidates:
+            if (
+                candidate.lower() != self.word
+                and sorted(candidate.lower()) == sorted_word
+            ):
+                matches.append(candidate)
+
+        return matches
